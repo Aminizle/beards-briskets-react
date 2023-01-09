fabc2d03cdcb9ea1aabacc099f40588ef680d886
Ameen Mohiyuddin <57292718+Aminizle@users.noreply.github.com>
2023-01-08 19:05:39 -0500

    added basic functionality

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import backgroundVideo from '../assets/BrisketCut.mp4'
 
 const Main = () => {
-  return (
-    <div>
-    asdasda
-    </div>
-  )
+    return (
+        <div className='main'>
+        <div className='overlay'></div>
+
+            <video src={backgroundVideo} autoPlay loop muted/>
+            <div className='content'>
+                <h1>Beards & Briskets</h1>
+                <p>About to set Northern Virginia ABLAZE!</p>
+            </div>
+        </div>
+    )
 }
 
 export default Main
