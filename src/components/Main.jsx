fd26af9ad724d64e1ecdff0836310bf2e28ca0b3
Ameen Mohiyuddin <57292718+Aminizle@users.noreply.github.com>
2023-01-23 16:52:48 -0500

    added favicon and logo

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,19 +1,20 @@
-import React from 'react'
-import backgroundVideo from '../assets/BrisketCut.mp4'
+import React from "react";
+import backgroundVideo from "../assets/BrisketCut.mp4";
+import logo from "../assets/BAB - Transparent White.png";
 
 const Main = () => {
-    return (
-        <div className='main'>
-        <div className='overlay'></div>
+  return (
+    <div className="main">
+      <div className="overlay"></div>
 
-            <video src={backgroundVideo} autoPlay loop muted/>
-            <div className='content'>
-                <h1>Beards & Briskets</h1>
-                <p>About to set Northern Virginia ABLAZE!</p>
-                
-            </div>
-        </div>
-    )
-}
+      <video src={backgroundVideo} autoPlay loop muted />
 
-export default Main
+      <div className="content">
+        <img src={logo} className="img" />
+        <p>About to set Northern Virginia ABLAZE!</p>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
